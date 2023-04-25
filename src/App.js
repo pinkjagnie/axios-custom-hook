cceec1a31da9ee21403139d463f817bbb8ecb389
pinkjagnie <olga.krzysztofik@gmail.com>
2023-04-25 19:48:10 +0200

    useAxiosFunction hook /w method fot GET & POST

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Jokes from './components/Jokes';
+import Posts from './components/Posts';
 
 function App() {
   return (
     <main className="App">
       <h1>useAxios hooks</h1>
       <Jokes />
+      <Posts />
     </main>
   );
 }
